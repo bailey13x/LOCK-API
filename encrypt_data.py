e79b4425f5f99b950e76a52cdc558d61e1f2b9c4
cbailey13x <96123312+cbailey13x@users.noreply.github.com>
2024-12-30 09:51:18 -0500

    new

--- encrypt_data.py
+++ encrypt_data.py
@@ -1,8 +1,8 @@
 from cryptography.fernet import Fernet
 
-key = b"NFEqx6ptrBHLwfMLGs1hOyjXpFwW9iRR8b2UvtJV5bs="  # Replace with the generated key
+key = b"<your key>"  # Replace with the generated key
 cipher = Fernet(key)
-encrypted_data = cipher.encrypt(b"postgresql://lock_database_user:5jg2OaPfPdQCr06lmEpw6burFA6zEln4@dpg-ctlam61opnds73858d9g-a/lock_database")
+encrypted_data = cipher.encrypt(b"<your DATABASE_URL_ENCRYPTED>")
 print(f"Encrypted Data: {encrypted_data.decode()}")
 
 
